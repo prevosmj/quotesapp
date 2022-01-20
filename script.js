df9edd55764512f7a99c0178a67db5d5683eb28d
prevosmj <69904719+prevosmj@users.noreply.github.com>
2022-01-20 15:18:53 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -21,10 +21,46 @@ const quotes = [
     }
 ];
 
-const newQ = function(){
+const warm = [
+    {
+        "c": "#d92e44"
+    },
+    {
+        "c": "#ed07c3"
+    },
+    {
+        "c": "#e06f1d"
+    },
+    {
+        "c": "#fafc58"
+    },
+    {
+        "c": "#c98ba9"
+    }
+]
+
+const cold = [
+    {
+        "c": "#4bbd31"
+    },
+    {
+        "c": "#0ad174"
+    },
+    {
+        "c": "#36a0d1"
+    },
+    {
+        "c": "#091596"
+    },
+    {
+        "c": "#6d0eb5"
+    }
+]
+
+function newQ() {
 
     let randnum = Math.floor(Math.random() * quotes.length);
-    console.log(randnum);
+    console.log("Quote: " + randnum);
 
     const currQ = quotes[randnum].q;
     const currA = quotes[randnum].a;
@@ -32,6 +68,16 @@ const newQ = function(){
     document.getElementById("text").innerHTML=currQ;
     document.getElementById("author").innerHTML="~ "+currA;
 
+    let randwarm = Math.floor(Math.random() * warm.length);
+    console.log("Warm: " + randwarm);
+    let randcold = Math.floor(Math.random() * cold.length);
+    console.log("Cold: " + randcold);
+
+    const currW = warm[randwarm].c;
+    const currC = cold[randcold].c;
+    let bkgrd = document.getElementById("gradient");
+
+    bkgrd.style.background = "linear-gradient(to bottom, " + currW + ", " + currC + ")";
 }
 
 newQ();
